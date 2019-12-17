99af69364339e6790a3c7a44c9b029fb21c76268
Emiliya <emiliya@Emiliyas-MacBook-Pro.local>
2019-12-17 16:02:18 -0500

    initial

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 export default function Header() {
   return (
     <Fragment>
-      <h1>Hiii</h1>
+      {/* <h1>Hiii</h1> */}
     </Fragment>
   );
 }
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-  res.sendFile()
-});
 
+app.use(express.static(path.join('client/public')));
+
+//Serve Statick assets if in production
+
+if (process.env.NODE_ENV === 'production') { 
+  app.use(express.static('client/public'));
+  app.get("*", (req, res) => { 
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
