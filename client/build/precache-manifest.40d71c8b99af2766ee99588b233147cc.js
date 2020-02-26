606e37966a5a256227bead49ba7731d0d9293791
Emiliya <emiliya@Emiliyas-MacBook-Pro.local>
2020-02-26 14:52:22 -0600

    heroku

---
+++ client/build/precache-manifest.40d71c8b99af2766ee99588b233147cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3ba7cc9e1a155dcce47caa188b8441",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6962dfe8e0439059f35c",
+    "url": "/static/js/2.401f4b95.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.401f4b95.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d212bdcc01c9a5e34ee",
+    "url": "/static/js/main.caa7d74b.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  }
+]);
